3e027fe6e37a7051d68f4a6d7c9604855c81d449
ozen-hub <info.ozenhub@gmail.com>
2022-12-23 22:46:34 +0530

    19

---
+++ src/app/components/delete-customer/delete-customer.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {HttpClient} from "@angular/common/http";
+
+@Component({
+  selector: 'app-delete-customer',
+  templateUrl: './delete-customer.component.html',
+  styleUrls: ['./delete-customer.component.scss']
+})
+export class DeleteCustomerComponent implements OnInit {
+
+
+  customerForm = new FormGroup({
+    id: new FormControl('', Validators.required),
+    name: new FormControl('',Validators.required),
+    address: new FormControl('',Validators.required),
+    salary: new FormControl(0,Validators.required),
+  })
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  findCustomer(){
+    let url = "http://localhost:8000/api/v1/customer/"+this.customerForm.get('id')?.value;
+
+    this.http.get<any>(url).subscribe(response=>{
+      this.customerForm.patchValue({
+        name:response.data.name,
+        address:response.data.address,
+        salary:response.data.salary,
+      })
+    })
+
+  }
+
+
+  deleteCustomer() {
+    let url = "http://localhost:8000/api/v1/customer/remove/"+this.customerForm.get('id')?.value;
+
+
+
+    this.http.delete(url).subscribe(responseData => {
+      console.log(responseData);
+      alert('Deleted!');
+      this.customerForm.reset();
+    });
+  }
+}
