04a6b582a9a54a7ac1835e782679bd49a8d95f8e
ozen-hub <info.ozenhub@gmail.com>
2022-12-23 20:50:56 +0530

    11

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatTabsModule} from "@angular/material/tabs";
+import { NewCustomerComponent } from './components/new-customer/new-customer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewCustomerComponent
   ],
   imports: [
     BrowserModule,
