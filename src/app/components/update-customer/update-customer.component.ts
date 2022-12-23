51e32a3fae88c9360ad2314351c431a0bbcaf834
ozen-hub <info.ozenhub@gmail.com>
2022-12-23 22:22:06 +0530

    17

---
+++ src/app/components/update-customer/update-customer.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {HttpClient} from "@angular/common/http";
+
+@Component({
+  selector: 'app-update-customer',
+  templateUrl: './update-customer.component.html',
+  styleUrls: ['./update-customer.component.scss']
+})
+export class UpdateCustomerComponent implements OnInit {
+
+
+  customerForm = new FormGroup({
+    id: new FormControl('', Validators.required),
+    name: new FormControl('',Validators.required),
+    address: new FormControl('',Validators.required),
+    salary: new FormControl(0,Validators.required),
+  })
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  findCustomer(){
+    let url = "http://localhost:8000/api/v1/customer/"+this.customerForm.get('id')?.value;
+
+    this.http.get<any>(url).subscribe(response=>{
+      this.customerForm.patchValue({
+        name:response.data.name,
+        address:response.data.address,
+        salary:response.data.salary,
+      })
+    })
+
+  }
+
+
+  updateCustomer() {
+
+  }
+}
